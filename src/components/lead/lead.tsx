61b0a44bcf2c1372d978849aac1c1ac8713c0808
MrDrog <fireman3085@mail.ru>
2023-04-25 19:58:29 +0300

    fix: made new fonts, new grid for projects

--- src/components/lead/lead.tsx
+++ src/components/lead/lead.tsx
@@ -1,25 +1,25 @@
 import './lead.scss'
 import myPhoto from '../../images/MyPhoto.png'
+import { Link as LinkScroll } from 'react-scroll'
 
 function Lead() {
 
     return (
         <section className='lead' id='lead'>
             <div className='lead__info'>
-                <p className='lead__info__text lead__text_size_middle'>Hello, It's Me</p>
-                <p className='lead__info__text lead__text_size_big'>IGOR DROGAITSEV</p>
-                <p className='lead__info__text lead__text_size_middle'>And I am Frontend Developer</p>
-                <p className='lead__info__text lead__text_size_min'>5 years I work with interfaces and data visualization and 2 years I develop in Front-end.</p>
-                <p className='lead__info__text lead__text_size_min'>Most of all I appreciate WIN-WIN cooperation. From my side, I try to constantly learn in order to be useful for the company. In projects I'm looking for the opportunity to challenge and develop my skills.</p>
-                <p className='lead__info__text lead__text_size_min'>This portfolio helps me showcase my values and leave contacts to connect with me. Welcome!</p>
+                <p className='lead__info__subtitle'>Hello, It's Me</p>
+                <p className='lead__info__title'>IGOR DROGAITSEV</p>
+                <p className='lead__info__subtitle'>And I am Frontend Developer</p>
+                <p className='lead__info__text'>5 years I work with interfaces and data visualization and 2 years with Front-end.</p>
+                <p className='lead__info__text'>Most of all I appreciate WIN-WIN cooperation. From my side, I try to constantly learn to be useful for the company. In projects I'm looking for the opportunity to challenge and develop my skills.</p>
+                <p className='lead__info__text'>This portfolio helps me to show my projects and leave contacts. Welcome!</p>
                 <div className='lead__info__link-container'>
-                    <a className='lead__info__link' href=''>Download CV</a>
-                    <a className='lead__info__link' href=''>Contacts</a>
+                    <a className='lead__info__link' target='blanc' href='https://docs.google.com/document/d/1YeKjZl2yOtwuFuFGLMII5-mF98bF1GZ7XcCSACxD6aQ/edit'>Download CV</a>
+                    <LinkScroll className='lead__info__link' to='contacts' spy={true} smooth={true} offset={-80} duration={500}>Contacts</LinkScroll>
                 </div>
             </div>
-            <div className='lead__img-container'>
-                <img className='lead__img-container__photo' src={myPhoto} alt='photo' />
-            </div>
+            <img className='lead__img-container__photo' src={myPhoto} alt='photo' />
+
         </section>
     )
 }
--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -11,11 +11,13 @@ function Project(props: projectProps) {
     return (
         <div className='project'>
             <p className='project__title'>{props.project.name}</p>
-            <div className='project__desktop'>
-                <img className='project__desktop__img' src={props.project.desktopImg} alt="desktop screenshot" />
-                <div className='project__desktop__text-container'>
+            <div className='project__block-container'>
+                <div className='project__img-container'>
+                    <img className='project__desktop-img' src={props.project.desktopImg} alt="desktop screenshot" />
+                </div>
+                <div className='project__text-container'>
                     <p className='project_subtitle'>ABOUT</p>
-                    <p className='project__text'><span className='project__span'>The main goal of the project: </span>{props.project.about}</p>
+                    <p className='project__text'><span className='project__span'>The main project goal: </span>{props.project.about}</p>
                     <ul className='project__ul-text'>
                         {props.project.aboutPoints.map(point => {
                             return <li className='project__text'>{point}</li>
@@ -23,8 +25,11 @@ function Project(props: projectProps) {
                     </ul>
                 </div>
             </div>
-            <div className='project__mobile'>
-                <div className='project__mobile__text-container'>
+            <div className='project__block-container'>
+                <div className='project__img-container'>
+                    <img className='project__mobile-img' style={{ backgroundImage: `url(${props.project.mobileImg})` }} src={mocMobile} alt="mobile screenshot" />
+                </div>
+                <div className='project__text-container'>
                     <p className='project_subtitle'>TECHNOLOGIES</p>
                     <p className='project__text'><span className='project__span'>The main technical goal: </span>{props.project.techologies}</p>
                     <ul className='project__ul-text'>
@@ -35,7 +40,7 @@ function Project(props: projectProps) {
                     <a className='project__link' href={props.project.githubLink}>GitHub</a>
                     <a className='project__link' href={props.project.webpageLink}>Open site</a>
                 </div>
-                <img className='project__mobile__img' style={{ backgroundImage: `url(${props.project.mobileImg})` }} src={mocMobile} alt="mobile screenshot" />
+
             </div>
         </div>
     )
